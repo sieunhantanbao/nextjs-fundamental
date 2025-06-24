f97c75d537ab843df759ce002083900413619ffc
Anh Nguyen Sieu <sieunhantanbao@gmail.com>
2025-06-24 09:33:58 +0700

    Update HTML metadata

--- pages/_document.js
+++ pages/_document.js
@@ -4,12 +4,17 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <meta charset="utf-8" />
+        <title>News - a CMS</title>
+        <meta name="description" content />
+        <meta name="author" content />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"></meta>
         {/* Preload critical scripts */}
         <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossOrigin="anonymous" />
         <script src="https://unpkg.com/imagesloaded@5/imagesloaded.pkgd.min.js" />
         <script src="https://unpkg.com/masonry-layout@4/dist/masonry.pkgd.min.js" />
       </Head>
-      <body>
+      <body id="top">
         <Main />
         <NextScript />
       </body>
